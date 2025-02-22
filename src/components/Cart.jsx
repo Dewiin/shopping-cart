b83ec689a8e099583d116717e33ded705e265011
Devin Xie <devinxie418@gmail.com>
2025-02-22 18:37:10 -0500

    finished shopping cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,10 @@
 import { Navbar } from "./Navbar";
 import "../styles/cart.css";
+import { useState } from "react";
 
 export function Cart( {cart, setCart} ) {
+    const [shipping, setShipping] = useState("free");
+
     function updateQuantity(product, newQuantity) {
         setCart((prevCart) => ({
             ...prevCart, [product]: {...prevCart[product], quantity: newQuantity}
@@ -21,7 +24,13 @@ export function Cart( {cart, setCart} ) {
             total += (cart[product].price * cart[product].quantity)
         ));
 
-        return total.toFixed(2);
+        return total;
+    }
+
+    const shippingOptions = {
+        "free": {label: "Free Delivery (7-14 days)", price: 0},
+        "standard": {label: "Standard Delivery (3-5 days) - $5.00", price: 5.00},
+        "express": {label: "Express Delivery (1-2 days) - $8.50", price: 8.50},
     }
 
     return (
@@ -53,11 +62,35 @@ export function Cart( {cart, setCart} ) {
                     </div>
                 </div>
                 <div className="cart-prices">
+                    <div>
                         <h2>Summary</h2>
                         <div className="cart-total">
                             <p>Items × {Object.keys(cart).length} </p>
-                            <p>{getTotalPrice()}</p>
+                            <p>$ {getTotalPrice().toFixed(2)}</p>
                         </div>
+                        <div className="cart-shipping">
+                            <p>SHIPPING</p>
+                            <select name="shipping-type" onChange={(e) => setShipping(e.target.value)}>
+                                <option value="free">{shippingOptions["free"].label}</option>
+                                <option value ="standard">{shippingOptions["standard"].label}</option>
+                                <option value="express">{shippingOptions["express"].label}</option>
+                            </select>
+                        </div>
+                        <div className="cart-discount">
+                            <p>DISCOUNT CODE</p>
+                            <div className="input-container">
+                                <input type="text"></input>
+                                <span className="input-arrow">→</span>
+                            </div>
+                        </div>
+                        <div className="cart-end-total">
+                            <p>TOTAL PRICE</p>
+                            <p>$ {(getTotalPrice() + shippingOptions[shipping].price).toFixed(2)}</p>
+                        </div>
+                        <div className="checkout-button">
+                            <button>CHECKOUT</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
